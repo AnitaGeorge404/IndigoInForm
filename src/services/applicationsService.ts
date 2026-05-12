e8760aa1eff3ec8c14e2e26eb0ad74397a1cf860
Anita George <anitageorge1806@gmail.com>
2026-05-12 16:54:31 +0530

    fixed errors caused dur to diff routes in backend

--- src/services/applicationsService.ts
+++ src/services/applicationsService.ts
@@ -4,11 +4,19 @@ export const submitApplication = async (data: any) => {
   try {
     console.log("SENDING APPLICATION DATA:", data);
 
-    const response = await api.post('/application/submit', data);
+    const payload = {
+      name: data.name,
+      email: data.email,
+      position: data.position,
+      description: data.message
+    };
+
+    const response = await api.post('/application', payload);
 
     console.log("APPLICATION SUCCESS:", response.data);
 
     return response.data;
+
   } catch (error: any) {
     console.error(
       "APPLICATION ERROR:",
--- src/services/jobsService.ts
+++ src/services/jobsService.ts
@@ -1,6 +1,6 @@
 import api from '../api/axios';
 
 export const getJobs = async () => {
-  const response = await api.get('/job/jobs');
+  const response = await api.get('/jobs');
   return response.data;
 };
