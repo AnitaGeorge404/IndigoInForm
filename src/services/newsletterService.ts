c5b75111b0f63ddbd02ea87c2af106c936c290e8
Anita George <anitageorge1806@gmail.com>
2026-05-13 13:10:59 +0530

    fixed errors

--- src/services/newsletterService.ts
+++ src/services/newsletterService.ts
@@ -1,6 +1,6 @@
 import api from '../api/axios';
 
 export const subscribeNewsletter = async (email: string) => {
-  const response = await api.post('/newsletter', { email });
+  const response = await api.post('/subscribe', { email });
   return response.data;
 };
